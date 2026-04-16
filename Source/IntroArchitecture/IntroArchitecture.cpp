69d47bcb490fa119687466191b49062d6e4704ab
Kenneth McLachlan <121731402+KennethMcLachlan@users.noreply.github.com>
2026-04-15 18:37:31 -0700

    Initial commit with added plugins

---
+++ Source/IntroArchitecture/IntroArchitecture.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IntroArchitecture.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IntroArchitecture, "IntroArchitecture" );
