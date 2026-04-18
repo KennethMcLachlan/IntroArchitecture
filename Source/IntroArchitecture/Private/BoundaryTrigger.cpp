9e1888889ee754e86c4ccc61732ef16f617bea45
KennethMcLachlan <kennethmclachlan11@gmail.com>
2026-04-18 15:56:49 -0700

    Created BoundaryTrigger Class

---
+++ Source/IntroArchitecture/Private/BoundaryTrigger.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BoundaryTrigger.h"
+
+// Sets default values for this component's properties
+UBoundaryTrigger::UBoundaryTrigger()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UBoundaryTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UBoundaryTrigger::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
